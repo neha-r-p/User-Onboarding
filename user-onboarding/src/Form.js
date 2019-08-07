28f7eac0f0707c9a6035423ad0e8be1055f8d8d8
Neha Patel <neha.r.p@gmail.com>
2019-08-07 16:03:26 -0700

    working on displaying data to the screen with hooks

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -10,18 +10,18 @@ const NewUserForm = ({ errors, touched, values }) => {
       <Form>
         <Field type="text" name="name" placeholder="Name" />
         {touched.name && errors.name && <p className="error">{errors.name}</p>}
-        
+
         <Field type="text" name="email" placeholder="E-mail" />
         {touched.email && errors.email && (
           <p className="error">{errors.email}</p>
         )}
-        
+
         <Field type="text" name="password" placeholder="Password" />
         {touched.password && errors.password && (
           <p className="error">{errors.password}</p>
         )}
         <label>
-          I agree with the Terms of Service.
+          I accept the Terms of Service.
           <Field type="checkbox" name="terms" />
           <span />
         </label>
@@ -42,9 +42,12 @@ const FormikNewUserForm = withFormik({
   },
 
   validationSchema: Yup.object().shape({
-    name: Yup.string().required(),
-    email: Yup.string().required(),
-    password: Yup.string().required()
+    name: Yup.string().required("Your name is required."),
+    email: Yup.string().required("Your e-mail is required."),
+    password: Yup.string()
+      .min(6, "Your password must be at least 6 characters.")
+      .required("Your password is required."),
+    terms: Yup.boolean(true).required('You must accept the terms of service.')
   }),
 
   handleSubmit(values) {
