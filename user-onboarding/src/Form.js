7dbabdc6927aae7be9752e10ec7849de5acee2ec
Neha Patel <neha.r.p@gmail.com>
2019-08-07 14:53:49 -0700

    Form.js added basic elements for form

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Form, Field, withFormik } from 'formik';
 
 const Form = () => {
     return(
-        <div>
-            
+        <div className="newUserForm">
+            <Form>
+                <Field></Field>
+                <Field></Field>
+                <Field></Field>
+                <Field></Field>
+            </Form>
+
         </div>
     )
 }
