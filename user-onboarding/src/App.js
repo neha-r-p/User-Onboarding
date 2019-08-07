367552dbcda8a5a8339abae775be43fe35c9d73c
Neha Patel <neha.r.p@gmail.com>
2019-08-07 15:12:31 -0700

    switched over to npm bc yarn wasnt installing formik

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Form from './Form';
+import NewUserForm from './Form';
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <NewUserForm />
     </div>
   );
 }
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import { Form, Field, withFormik } from 'formik';
+import React from "react";
+import { Form, Field } from "formik";
 
-const Form = () => {
-    return(
-        <div className="newUserForm">
-            <Form>
-                <Field></Field>
-                <Field></Field>
-                <Field></Field>
-                <Field></Field>
-            </Form>
+const NewUserForm = () => {
+  return (
+    <div className="newUserForm">
+      <h2>New User Form</h2>
+      <Form>
+        <Field type="text" name="name" placeholder="Name" />
+        <Field type="text" name="email" placeholder="E-mail" />
+        <Field type="text" name="password" placeholder="Password" />
+        <label>
+          <Field type="checkbox" name="terms" />
+          <span />
+        </label>
+        <button type="submit">Submit</button>
+      </Form>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Form;
+export default NewUserForm;
